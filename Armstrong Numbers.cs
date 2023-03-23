09f62de7d0e553eaef2362d5017d1849a9f31054
vidhyaShree886 <vidhyashree886@gmail.com>
2023-03-23 12:07:05 -0400

    Adding Armstrong Numbers to git

---
+++ Armstrong Numbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, rem, temp, sum = 0;
+            Console.WriteLine("check if armstrong number or not");
+            Console.Write("enter your number to check:");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            temp = number;
+            while (number > 0)
+
+            {
+                rem = number % 10;
+                sum = sum + (rem * rem * rem);
+                number = number / 10;
+
+            }
+            if (temp == sum)
+
+                Console.WriteLine(temp + " is an armstrong number");
+            else
+                Console.WriteLine(temp + " is not an armstrong number");
+            Console.ReadLine();
+        }
+             
+
+                         
+    }
+}
